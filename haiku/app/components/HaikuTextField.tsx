28f44f3ef35287f8b6f8e505af8d5147bc6b8600
Mathias Bøe <mrboen94@gmail.com>
2023-09-06 16:34:22 +0200

    Fixed formatting

--- haiku/app/components/HaikuTextField.tsx
+++ haiku/app/components/HaikuTextField.tsx
@@ -1,113 +1,135 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from "react";
 
 interface Particle {
+  x: number;
+  y: number;
+  alpha: number;
+  color: number[];
+  velocity: {
     x: number;
     y: number;
-    alpha: number;
-    color: number[];
-    velocity: {
-        x: number;
-        y: number;
-    };
+  };
 }
 
 const MAX_PARTICLES = 50;
 const PARTICLE_ALPHA_FADEOUT = 0.96;
 const PARTICLE_VELOCITY_RANGE = {
-    x: [0, 3], // controls the x direction particle velocity range
-    y: [-2.5, -1.5] // controls the y direction particle velocity range
+  x: [0, 3], // controls the x direction particle velocity range
+  y: [-2.5, -1.5], // controls the y direction particle velocity range
 };
 
 const MyAnimatedTextarea: React.FC = () => {
-    const canvasRef = useRef<HTMLCanvasElement>(null);
-    const particles = useRef<Particle[]>([]);
-    const [needsRedraw, setNeedsRedraw] = useState(false);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const particles = useRef<Particle[]>([]);
+  const [needsRedraw, setNeedsRedraw] = useState(false);
 
-    useEffect(() => {
-        const canvas = canvasRef.current;
-        if (!canvas) return;
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
 
-        const ctx = canvas.getContext('2d');
-        if (!ctx) return;
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
 
-        const drawFrame = () => {
-            if (particles.current.length) {
-                ctx.clearRect(0, 0, canvas.width, canvas.height);
-                particles.current.forEach((particle) => {
-                    particle.velocity.y += 0.075; // Gravity
-                    particle.x += particle.velocity.x;
-                    particle.y += particle.velocity.y;
-                    particle.alpha *= PARTICLE_ALPHA_FADEOUT;
+    const drawFrame = () => {
+      if (particles.current.length) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        particles.current.forEach((particle) => {
+          particle.velocity.y += 0.075; // Gravity
+          particle.x += particle.velocity.x;
+          particle.y += particle.velocity.y;
+          particle.alpha *= PARTICLE_ALPHA_FADEOUT;
 
-                    if (particle.alpha > 0.1) {
-                        ctx.fillStyle = `rgba(${particle.color.join(',')}, ${particle.alpha})`;
-                        ctx.fillRect(Math.round(particle.x - 1), Math.round(particle.y - 1), 3, 3);
-                    }
-                });
+          if (particle.alpha > 0.1) {
+            ctx.fillStyle = `rgba(${particle.color.join(",")}, ${
+              particle.alpha
+            })`;
+            ctx.fillRect(
+              Math.round(particle.x - 1),
+              Math.round(particle.y - 1),
+              3,
+              3,
+            );
+          }
+        });
 
-                particles.current = particles.current.filter((particle) => particle.alpha > 0.1);
-            }
+        particles.current = particles.current.filter(
+          (particle) => particle.alpha > 0.1,
+        );
+      }
 
-            if (particles.current.length > 0 || needsRedraw) {
-                requestAnimationFrame(drawFrame);
-            }
-        };
-
+      if (particles.current.length > 0 || needsRedraw) {
         requestAnimationFrame(drawFrame);
-    }, [needsRedraw]);
-
-    const spawnParticles = (x: number, y: number) => {
-        const numParticles = 5 + Math.round(Math.random() * 5);
-        for (let i = 0; i < numParticles; i++) {
-            const color = [255, 0, 0]; // Red color for demonstration
-            particles.current.push(createParticle(x, y, color));
-        }
-
-        if (!needsRedraw) {
-            setNeedsRedraw(true);
-        }
+      }
     };
 
-    const createParticle = (x: number, y: number, color: number[]): Particle => {
-        return {
-            x,
-            y,
-            alpha: 1,
-            color,
-            velocity: {
-                x: PARTICLE_VELOCITY_RANGE.x[0] + Math.random() * (PARTICLE_VELOCITY_RANGE.x[1] - PARTICLE_VELOCITY_RANGE.x[0]),
-                y: PARTICLE_VELOCITY_RANGE.y[0] + Math.random() * (PARTICLE_VELOCITY_RANGE.y[1] - PARTICLE_VELOCITY_RANGE.y[0])
-            }
-        };
+    requestAnimationFrame(drawFrame);
+  }, [needsRedraw]);
+
+  const spawnParticles = (x: number, y: number) => {
+    const numParticles = 5 + Math.round(Math.random() * 5);
+    for (let i = 0; i < numParticles; i++) {
+      const color = [255, 0, 0]; // Red color for demonstration
+      particles.current.push(createParticle(x, y, color));
+    }
+
+    if (!needsRedraw) {
+      setNeedsRedraw(true);
+    }
+  };
+
+  const createParticle = (x: number, y: number, color: number[]): Particle => {
+    return {
+      x,
+      y,
+      alpha: 1,
+      color,
+      velocity: {
+        x:
+          PARTICLE_VELOCITY_RANGE.x[0] +
+          Math.random() *
+            (PARTICLE_VELOCITY_RANGE.x[1] - PARTICLE_VELOCITY_RANGE.x[0]),
+        y:
+          PARTICLE_VELOCITY_RANGE.y[0] +
+          Math.random() *
+            (PARTICLE_VELOCITY_RANGE.y[1] - PARTICLE_VELOCITY_RANGE.y[0]),
+      },
     };
+  };
 
-    const handleInput = (e: React.FormEvent<HTMLTextAreaElement>) => {
-        const textarea = e.currentTarget;
-        const { left, top } = textarea.getBoundingClientRect();
-        const cursorPos = textarea.selectionStart;
-        const lines = textarea.value.substring(0, cursorPos).split("\n");
+  const handleInput = (e: React.FormEvent<HTMLTextAreaElement>) => {
+    const textarea = e.currentTarget;
+    const { left, top } = textarea.getBoundingClientRect();
+    const cursorPos = textarea.selectionStart;
+    const lines = textarea.value.substring(0, cursorPos).split("\n");
 
-        const lineNum = lines.length;
-        const colNum = lines[lines.length - 1].length;
+    const lineNum = lines.length;
+    const colNum = lines[lines.length - 1].length;
 
-        // You may need to adjust these based on your specific styling
-        const charWidth = 10; // Approximate width of a character in pixels
-        const lineHeight = 20; // Approximate line height in pixels
+    // You may need to adjust these based on your specific styling
+    const charWidth = 10; // Approximate width of a character in pixels
+    const lineHeight = 20; // Approximate line height in pixels
 
-        const x = left + colNum * charWidth;
-        const y = top + (lineNum - 1) * lineHeight;
+    const x = left + colNum * charWidth;
+    const y = top + (lineNum - 1) * lineHeight;
 
-        spawnParticles(x, y);
-    };
+    spawnParticles(x, y);
+  };
 
-
-
-    return (
-        <div style={{ position: 'relative' }}>
-            <textarea onInput={handleInput} style={{ zIndex: 1 }} className={"font-mono h-64 leading-5 w-full"} />
-            <canvas ref={canvasRef} style={{ position: 'absolute', top: 0, left: 0, pointerEvents: 'none' }} width={window.innerWidth} height={window.innerHeight}></canvas>
-        </div>
-    );
+  return (
+    <div style={{ position: "relative" }}>
+      <textarea
+        onInput={handleInput}
+        style={{ zIndex: 1 }}
+        className={"font-mono h-64 leading-5 w-full"}
+      />
+      <canvas
+        ref={canvasRef}
+        style={{ position: "absolute", top: 0, left: 0, pointerEvents: "none" }}
+        width={window.innerWidth}
+        height={window.innerHeight}
+      ></canvas>
+    </div>
+  );
 };
 
 export default MyAnimatedTextarea;
--- haiku/app/lib/verifyHaiku.ts
+++ haiku/app/lib/verifyHaiku.ts
@@ -1,42 +1,42 @@
-import { syllable } from 'syllable';
+import { syllable } from "syllable";
 
 export function verifyHaiku(haiku: string): string[] | null {
-    const lines = haiku.trim().split('\n');
-    const syllableCounts = lines.map((line) => countSyllables(line));
+  const lines = haiku.trim().split("\n");
+  const syllableCounts = lines.map((line) => countSyllables(line));
 
-    const errors: string[] = [];
+  const errors: string[] = [];
 
-    // Haiku should have three lines
-    if (lines.length !== 3) {
-        errors.push('Haiku should have exactly three lines.');
-    }
+  // Haiku should have three lines
+  if (lines.length !== 3) {
+    errors.push("Haiku should have exactly three lines.");
+  }
 
-    // Haiku should have 5, 7, and 5 syllables respectively
-    if (syllableCounts[0] !== 5) {
-        errors.push('First line should have 5 syllables.');
-    }
-    if (syllableCounts[1] !== 7) {
-        errors.push('Second line should have 7 syllables.');
-    }
-    if (syllableCounts[2] !== 5) {
-        errors.push('Third line should have 5 syllables.');
-    }
+  // Haiku should have 5, 7, and 5 syllables respectively
+  if (syllableCounts[0] !== 5) {
+    errors.push("First line should have 5 syllables.");
+  }
+  if (syllableCounts[1] !== 7) {
+    errors.push("Second line should have 7 syllables.");
+  }
+  if (syllableCounts[2] !== 5) {
+    errors.push("Third line should have 5 syllables.");
+  }
 
-    return errors.length > 0 ? errors : null;
+  return errors.length > 0 ? errors : null;
 }
 
 function countSyllables(line: string): number {
-    const words = line.trim().split(' ');
-    let count = 0;
-    words.forEach((word) => {
-        count += countSyllablesInWord(word);
-    });
-    return count;
+  const words = line.trim().split(" ");
+  let count = 0;
+  words.forEach((word) => {
+    count += countSyllablesInWord(word);
+  });
+  return count;
 }
 
 // Helper function to count syllables in a word (naive implementation)
 function countSyllablesInWord(word: string): number {
-    const syllableCount = syllable(word);
+  const syllableCount = syllable(word);
 
-    return syllableCount;
+  return syllableCount;
 }
